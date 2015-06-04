f6a6ce6a72e607c8a5ec5828f157a01a1d420544
spirAde <spirAde@yandex.ru>
2015-06-04 14:05:44 +0500

    Front. Added ng-infinity-scroll.

--- frontend/web/scripts/component/bathhouse-list/provider/data-storage.factory.js
+++ frontend/web/scripts/component/bathhouse-list/provider/data-storage.factory.js
@@ -47,7 +47,7 @@ function dataStorage($rootScope, $http, $q, $timeout, userStorage, CONSTANTS) {
 		var _this = this;
 		var pages;
 
-		return $http.get('http://api.ordr.ru/rooms?cityId=' + cityId)
+		return $http.get('http://api.ordr.ru/rooms?cityId=' + cityId + '&expand=bathinfo')
 			.then(function(response) {
 
 				pages = _.range(2, response.data._meta.pageCount + 1);
@@ -64,7 +64,7 @@ function dataStorage($rootScope, $http, $q, $timeout, userStorage, CONSTANTS) {
 
 				_.forEach(pages, function(page) {
 
-					$http.get('http://api.ordr.ru/rooms?cityId=' + cityId + '&page=' + page)
+					$http.get('http://api.ordr.ru/rooms?cityId=' + cityId + '&page=' + page + '&expand=bathinfo')
 						.then(function(response) {
 
 							_this.rooms = _.union(_this.rooms, _.map(response.data.items, _addProperties));
