11731063d3ae1fc35bd6ea3f393ae92041ea261a
lwdmk <dmitryklkv@gmail.com>
2015-08-09 23:47:16 +0500

    backend delete fix

--- api/components/actions/FixedOptionsAction.php
+++ api/components/actions/FixedOptionsAction.php
@@ -8,17 +8,14 @@ use yii\rest\OptionsAction;
 class FixedOptionsAction extends OptionsAction
 {
 
-    public $collectionOptions = ['GET', 'POST', 'HEAD', 'OPTIONS'];
-    public $resourceOptions = ['GET', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'];
-
-    
     public function run($id = null)
     {
         if (Yii::$app->getRequest()->getMethod() !== 'OPTIONS') {
             Yii::$app->getResponse()->setStatusCode(405);
         }
-        $options = $id === null ? $this->collectionOptions : $this->resourceOptions;
+        $options = ['GET', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'];
         Yii::$app->getResponse()->getHeaders()->set('Access-Control-Allow-Headers', 'accept, content-Type, authorization');
         Yii::$app->getResponse()->getHeaders()->set('Allow', implode(', ', $options));
     }
+    
 }
